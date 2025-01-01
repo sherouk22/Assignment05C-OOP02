d1c3b2e68d6207a12ed0acfa893680e78373a6c5
sherouk22 <sheroukmohy558@gmail.com>
2025-01-01 22:53:23 +0200

    Add project files.

---
+++ Assignment05OOP02/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment05OOP02
+{
+    public struct Person
+    {
+        public string Name;
+        public int Age;
+
+        public  Person(string name ,int age)
+        {
+            Name = name;
+            Age = age;  
+        }
+    }
+}
---
+++ Assignment05OOP02/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment05OOP02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+             Person[] people = new Person[3];
+
+            people[0] = new Person("Ahmed",22);
+            people[1] = new Person("Sherouk", 23);
+            people[2] = new Person("Eman", 19);
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine($" name-->{person.Name}  Age-->{person.Age} ");
+            }
+        }
+    }
+}
